bf3cef8f2f63151557f53a15a88f744c69029e48
buc0025 <stanley.yang001@gmail.com>
2021-03-25 19:00:19 -0400

    Testing first React Native project

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={{fontSize: 20}}>Lorem ipsum dolor sit amet. Lorem ipsum dolor st.Lorem ipsum dolor sit amet.
+        </Text>
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -18,4 +24,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    // fontSize: 20
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  }
 });
