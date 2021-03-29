fb965c76b223edae07c57f2f7b50f2c639a0e558
buc0025 <stanley.yang001@gmail.com>
2021-03-28 21:28:49 -0400

    Flatlist and touchable components in React Native 03/28/2021
    -Flatlist is similar to RecyclerView in Android while ScrollView is similar to ListView
    -Turning components into touchables is similar to onClickListeners in Android

--- App.js
+++ App.js
@@ -1,32 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Stanley');
-  const [person, setPerson] = useState({ name: 'mario', age: 40});
-  const [age, setAge] = useState(34);
-
   const [people, setPeople] = useState([
-    { name: 'stanley', key: '1'},
-    { name: 'nancy', key: '2'},
-    { name: 'jason', key: '3'},
-    { name: 'dan', key: '4'},
-    { name: 'steveny', key: '5'},
-    { name: 'mandy', key: '6'},
-  ])
+    { name: 'stanley', id: '1'},
+    { name: 'nancy', id: '2'},
+    { name: 'jason', id: '3'},
+    { name: 'dan', id: '4'},
+    { name: 'steven', id: '5'},
+    { name: 'mandy', id: '6'},
+    { name: 'amy', id: '7'}
+  ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id)
+    })
+  }
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        { people.map((item) => {
-          return (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          )
-        })}
-      </ScrollView>
+      
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>          
+        )}
+      />
 
     </View>
   )
@@ -37,7 +43,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 40,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
     // alignItems: 'center',
     // justifyContent: 'center',
   },
@@ -45,28 +51,7 @@ const styles = StyleSheet.create({
     marginTop: 24,
     padding: 30,
     backgroundColor: 'pink',
-    fontSize: 24
-  },
-  buttonContainer: {
-    marginTop: 20
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200
-  },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-    // fontSize: 20
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20
+    fontSize: 24,
+    marginHorizontal: 10
   }
 });
